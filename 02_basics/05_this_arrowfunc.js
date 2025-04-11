35869aec5b84432717ca19bf10e2c1b8c0e6c7df
Avanish Shukla <avanishshukla2021@gmail.com>
2025-04-11 16:02:36 +0000

    All about this function in arrow and reular function.

---
+++ 02_basics/05_this_arrowfunc.js
@@ -0,0 +1,116 @@
+//this -> jeyword that refers to current context
+
+// console.log(this)
+// **** Interview asked -> But when this runs in browser ... The global object that we get is Window Object 
+// in node env global object is {} empty object
+
+
+// function, this, arrow function and others INTERVIEW
+
+function temp()
+{
+    let username = "avanish"
+    console.log(this)
+    // console.log(this.username) // gives undefined Sol THIS CANNOT BE USED IN functions IT CAN ONLY BE USED IN OBJECTS
+}
+
+temp()
+
+
+// **** in arrow function->
+
+// FUNCTION CORRECTLY DEFINED BUT IT CANNOT BE CALLED. So this is not suitable ->
+
+// () => {
+//     let username = "aman"
+//     console.log(username)
+// }
+
+// So store it in some variable and then call
+const l = () => {
+        let username = "aman"
+        console.log(username)
+    }
+
+    l()
+
+4. // ********************FAMOUS INTERVIEW QUES ****************
+// Regular functions create their own this context based on the calling object.
+
+// Arrow functions DO NOT create their own this — they use the this of the outer (enclosing) scope it means
+
+
+
+
+// ************* regular function
+
+// const user = {
+//     name: "Aman",
+//     age: 25,
+//     greet: function () {
+//         console.log(`Hi, my name is ${this.name} and I'm ${this.age} years old.`);
+//     }
+// };
+
+// user.greet(); // 👉 "Hi, my name is Aman and I'm 25 years old."
+
+// greet() is a regular function inside an object
+// this refers to the object (user) that called the function.
+
+
+// ************ arrow function ->
+
+const user1 = {
+    name: "Aman",
+        sayHi : () => {
+        console.log(`Hi, I am ${this.name}`); // undefined
+    }
+};
+
+console.log(user1.sayHi()) // Arrow function doesn't create its own this. It tries to use this.name, but this comes from the outer scope 
+
+
+// DIFFERENCE IN BEHAVIOUR OF THIS
+
+const user = {
+    name: "Aman",
+    greet: function() {
+        const sayHi = () => {
+            console.log(`Hi, I am ${this.name}`);
+        };
+
+        sayHi(); // ✅ Works!
+    }
+};
+
+user.greet(); // 👉 "Hi, I am Aman" 
+// it works because -> sayHi is an arrow function, so it doesn't have its own this. It inherits this from greet(), which is called on user
+ 
+
+
+
+
+
+
+
+// on other hand doing same in regular function will give error because->
+
+// sayHi() is defined under a function greet() and this cannot be used to access inside a function i.e
+
+// const user = {
+//     name: "Aman",
+//     greet: function() {
+//         const sayHi = function() {
+//             console.log(`Hi, I am ${this.name}`);
+//         };
+
+//         sayHi(); // ❌ `this` is undefined or window
+//     }
+// };
+
+// user.greet(); // 👉 "Hi, I am undefined"
+
+
+
+
+
